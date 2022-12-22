6bf5da8389046f4e36ce42a7a431bf3acad86c7b
aisyahnn <81240334+aisyahnn@users.noreply.github.com>
2022-12-22 22:44:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seleksi Kementerian Kelautan dan Perikanan</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class=" formContainer" style="margin-top: auto; margin-bottom: auto;">
+	<p align="center">
+    </p>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;800;900&family=Roboto:wght@100&family=Secular+One&display=swap" rel="stylesheet">
+            <br>
+			<h3> Selamat Datang! </h3>
+            <h4> Calon pegawai baru Kementrian Kelautan dan Perikanan</h4>
+            <p> Website Kementrian Kelautan dan Perikanan</p>
+            <br>
+
+            <div class="d-flex justify-content-center">
+                <a href="login-user.php"><button class="mr-3 btn btn-primary"> Peserta </button></a>
+                <a href="login-admin.php"><button class="btn btn-primary"> Admin</button></a>
+            </div>
+    </div>
+</body>
+</html>
+
